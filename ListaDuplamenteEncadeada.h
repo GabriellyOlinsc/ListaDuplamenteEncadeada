d66848d37e2e5b7573cf0006ada38615d6a4023d
Gabrielly Olinsc <gabrielly-04@outlook.com>
2023-09-07 18:32:08 -0300

    fix: insereLista

--- ListaDuplamenteEncadeada.h
+++ ListaDuplamenteEncadeada.h
@@ -1,9 +1,7 @@
 #ifndef LISTADUPLAMENTEENCADEADA_H_INCLUDED
 #define LISTADUPLAMENTEENCADEADA_H_INCLUDED
-
 #include <iostream>
 #include <fstream>
-#include <cstring>
 #include <cstdlib>
 using namespace std;
 
@@ -22,73 +20,62 @@ struct ListaDuplamenteEncadeada{
 };
 
 template<typename T>
-void leituraArquivo (ListaDuplamenteEncadeada<T> lista, string nomeArq){
-    T palavra;
-    int valor = 0;
+void leituraArquivoString (ListaDuplamenteEncadeada<T> &lista, string nomeArq, int ordemLista){
+    string palavra;
     char ch;
-    int coluna = 0;
+    int coluna;
+    ifstream arqSeq(nomeArq.c_str());
+    if(arqSeq.fail()){
+        cout<<"Erro na abertura do arquivo '"<<nomeArq<<"'!";
+        exit(0);}
+
+    while (arqSeq.get(ch)){
+        if(ch == ' ' or ch == '\n'){
+            if(coluna == ordemLista){
+                insereNaLista(lista,palavra,numeroDeElementos(lista)+1);
+            }
+           coluna++;
+           if(coluna > 3) coluna = 0;
+           palavra = "";
+        }else
+            palavra = palavra + ch;
+    }
+    arqSeq.close();
+}
+
+template< typename T>
+void leituraArquivoInt (ListaDuplamenteEncadeada<T> &lista, string nomeArq, int ordemLista){
+    string palavra;
+    char ch;
+    int coluna;
+    T elemento;
 
     ifstream arqSeq(nomeArq.c_str());
     if(arqSeq.fail()){
         cout<<"Erro na abertura do arquivo '"<<nomeArq<<"'!";
-        exit(0);
-    }
+        exit(0);}
 
     while (arqSeq.get(ch)){
         if(ch == ' ' or ch == '\n'){
-            switch (coluna){
-                case 0:
-                    //cout<<"\n\nnumero de voo : "<<palavra<<endl;
-                   // insereNaLista(lista, palavra);
-                    // insere lista1
-                    break;
-                case 1:
-                    //cout<<"local de origem: "<< palavra<<endl;
-                    //insereNaLista(lista, palavra);
-                    // insere lista2
-                    break;
-                case 2:
-                    //cout<<"local de destino: "<< palavra<<endl;
-                    insereNaLista(lista, palavra);
-                    // insere lista3
-                    break;
-                case 3:
-                    //cout<<"numero de lugares disponiveis: "<< palavra<<endl;
-                  //  insereNaLista(lista, palavra);
-                    // insere lista4
-                    break;
-                }
-            coluna ++;
-            if(coluna > 3)
-                coluna = 0;
-<<<<<<< HEAD
-            palavra = T();
-=======
-            palavra = "";
->>>>>>> df8b83d24d65c12d597561a20b9554a144529481
-
+            if(coluna == ordemLista){
+                elemento = atoi (palavra.c_str());
+                insereNaLista(lista,elemento,numeroDeElementos(lista)+1);
+            }
+           coluna++;
+           if(coluna > 3) coluna = 0;
+           palavra = "";
         }else
-        palavra = palavra + ch;
+            palavra = palavra + ch;
     }
     arqSeq.close();
 }
+
 
 template<typename T>
 void criaLista (ListaDuplamenteEncadeada<T> &lista){
     lista.cardinalidade = 0;
     lista.inicio = NULL;
     lista.fim = NULL;
-}
-
-template<typename T>
-int numeroDeElementos(ListaDuplamenteEncadeada<T> lista){
-    return lista.cardinalidade;
-}
-
-template<typename T>
-bool ehVazia(ListaDuplamenteEncadeada<T> lista)
-{
-    return lista.cardinalidade == 0;
 }
 
 template<typename T>
@@ -103,26 +90,57 @@ bool existeElemento(ListaDuplamenteEncadeada<T> lista, T elemento){
     }
     return false;
 }
-//ARRUMAR E COLOCAR INSERE NA POSICAO
-template<typename T>
-void insereNaLista(ListaDuplamenteEncadeada<T>& lista, T elemento) {
-    Nodo<T>* novoNodo = new Nodo<T>;
-    novoNodo->elemento = elemento;
-    novoNodo->proximo = nullptr;
 
-    if (lista.inicio == nullptr) {
-        novoNodo->anterior = nullptr;
-        lista.inicio = novoNodo;
-        lista.fim = novoNodo;
-    } else {
-        novoNodo->anterior = lista.fim;
-        lista.fim->proximo = novoNodo;
-        lista.fim = novoNodo;
+template<typename T>
+int numeroDeElementos(ListaDuplamenteEncadeada<T> lista){
+    return lista.cardinalidade;
+}
+
+template<typename T>
+bool ehVazia(ListaDuplamenteEncadeada<T> lista){
+    return lista.cardinalidade == 0;
+}
+
+template<typename T>
+void insereNaLista(ListaDuplamenteEncadeada<T>& lista, T elemento, int posicao) {
+   int contPosicao;
+   if(posicao < 1 || posicao > lista.cardinalidade + 1)
+        throw "POSICAO INVALIDA";
+
+    Nodo<T>* ptr = new Nodo<T>;
+    ptr->elemento = elemento;
+    ptr->proximo = NULL;
+    ptr->anterior = NULL;
+
+    if (lista.inicio == NULL){ //se for vazia
+        lista.inicio = ptr;
+        lista.fim = ptr;
+    }else{
+        Nodo<T>* p = lista.inicio;
+        if (posicao == 1){
+            ptr->proximo = lista.inicio;
+            p->anterior = ptr;
+            lista.inicio = ptr;
+        }else{
+            contPosicao = 1;
+            while (posicao - 1 != contPosicao){ //pois vamos inserir antes do nodo da posicao
+                p = p->proximo;
+                contPosicao++;
+            }
+            ptr->proximo = p->proximo;
+            p->proximo = ptr;
+            ptr->anterior = p;
+            if (ptr->proximo != NULL){
+                p = ptr->proximo;
+                p->anterior = ptr;
+            }
+            else
+                lista.fim = ptr;
+        }
     }
     lista.cardinalidade++;
 }
 
-//FALTA O RECUPERA POSICAO
 //FALTA O RETIRA ELEMENTO
 
 template<typename T>
@@ -144,7 +162,28 @@ T recuperaElemento(ListaDuplamenteEncadeada<T> &lista, int posicao) {
         contFim--;
     }
 }
-<<<<<<< HEAD
+
+template<typename T>
+int recuperarPosicao(ListaDuplamenteEncadeada<T> lista, T elemento){
+    Nodo<T> *p = lista.inicio, * q = lista.fim;
+    int index = 1, indexFim = lista.cardinalidade;
+
+    if(!existeElemento(lista,elemento)){
+        throw "Elemento nao existe!";
+    }
+    while(p != NULL || q!=NULL){
+        if(p->elemento == elemento)
+            return index;
+        if(p->elemento == elemento)
+            return indexFim;
+        q = q->anterior;
+        p = p->proximo;
+        index++;
+        indexFim--;
+    }
+}
+
+/*
 template<typename T>
 int recuperarPosicao(ListaDuplamenteEncadeada<T> lista, T e){
     Nodo<T> *p = lista.inicio;
@@ -158,33 +197,20 @@ int recuperarPosicao(ListaDuplamenteEncadeada<T> lista, T e){
 
     return index;
 }
-
-template<typename T>
-bool existeElemento(ListaDuplamenteEncadeada<T> lista, T elemento){
-    Nodo<T>* p = lista.inicio;
-    while(p != NULL){
-        if(p->elemento == elemento)
-            return true;
-        p = p->proximo;
-    }
-    return false;
-}
-=======
->>>>>>> df8b83d24d65c12d597561a20b9554a144529481
+*/
 
 template<typename T>
 void exibeLista(ListaDuplamenteEncadeada<T> lista)
 {
     for (Nodo<T>* p = lista.inicio; p != nullptr; p = p->proximo)
         cout << p->elemento << "\t";
+    cout<<endl;
 }
 
 template<typename T>
-void destroi(ListaDuplamenteEncadeada<T> &lista)
-{
+void destroiLista(ListaDuplamenteEncadeada<T> &lista){
     Nodo<T> *p;
-    while (lista.inicio != NULL)
-    {
+    while (lista.inicio != NULL){
         p = lista.inicio;
         lista.inicio = lista.inicio->proximo;
         delete p;
--- main.cpp
+++ main.cpp
@@ -3,72 +3,46 @@
 
 using namespace std;
 int main() {
-<<<<<<< HEAD
     string nomeArq, palavra = "RJ";
     cout<<"Informe o nome do arquivo (.txt): ";
     getline(cin,nomeArq);
 
     ListaDuplamenteEncadeada<int>numeroVoo;
-    ListaDuplamenteEncadeada<int> IDVoo;
     ListaDuplamenteEncadeada<string> localOrigem;
     ListaDuplamenteEncadeada<string> localDestino;
+    ListaDuplamenteEncadeada<int> assentos;
 
     criaLista(numeroVoo);
-    criaLista(IDVoo);
+    criaLista(assentos);
     criaLista(localOrigem);
     criaLista(localDestino);
 
-    //leituraArquivo(IDVoo,nomeArq);
-    leituraArquivo(localOrigem, nomeArq);
-    leituraArquivo(IDVoo, nomeArq);
+    leituraArquivoInt(numeroVoo,nomeArq,0);
+    leituraArquivoString(localOrigem, nomeArq,1);
+    leituraArquivoString(localDestino, nomeArq,2);
+    leituraArquivoInt(assentos, nomeArq,3);
 
     exibeLista(numeroVoo);
-    exibeLista(IDVoo);
     exibeLista(localOrigem);
     exibeLista(localDestino);
-
-    // cout << recuperaElemento(localOrigem, 1);
+    exibeLista(assentos);
 
     try{
-        recuperarPosicao(localOrigem, palavra);
+        cout<<"Posicao: "<<recuperarPosicao(localOrigem, palavra);
     }catch(char const* msg){
         cout << msg;
     }
 
     destroiLista(numeroVoo);
-    destroiLista(IDVoo);
+    destroiLista(assentos);
     destroiLista(localOrigem);
     destroiLista(localDestino);
-=======
-    string nomeArq, OLGA="OLA";
 
-    cout<<"Informe o nome do arquivo (.txt): ";
-    getline(cin,nomeArq);
 
-    ListaDuplamenteEncadeada<int> IDVOO;
-    criaLista(IDVOO);
-    ListaDuplamenteEncadeada<string> localOrigem;
-    criaLista(localOrigem);
-    ListaDuplamenteEncadeada<string> localDestino;
-    criaLista(localDestino);
-    ListaDuplamenteEncadeada<int> assentos;
-    criaLista(assentos);
-
-    leituraArquivo(localDestino,nomeArq);
-    exibeLista(localDestino);
-
-    cout<<numeroDeElementos(localDestino)<<endl;
-    try{
-        cout<<recuperaElemento(localDestino,4);
-    }catch(const char* msg){
-        cerr<<msg<<endl;
-    }
-
-    destroi(IDVOO);
-    destroi(localOrigem);
-    destroi(localDestino);
-    destroi(assentos);
->>>>>>> df8b83d24d65c12d597561a20b9554a144529481
+    destroiLista(numeroVoo);
+    destroiLista(assentos);
+    destroiLista(localOrigem);
+    destroiLista(localDestino);
 
     return 0;
 }
